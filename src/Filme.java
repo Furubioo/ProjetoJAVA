df484a8263765183e73fc5e69a69a4a530feb30f
Flávio Daniel <epodomicehe@gmail.com>
2026-03-07 11:28:58 -0300

    Create Filme.java
    Commit com os atributos, construtor e métodos getter e setter dos atributos(classe Filme)

---
+++ src/Filme.java
@@ -0,0 +1,46 @@
+public class Filme {
+    private String nome;
+    private int duracao;
+    private String sinopse;
+    private double valor;
+
+    public Filme(String nome, int duracao, String sinopse, double valor) {
+        this.nome = nome;
+        this.duracao = duracao;
+        this.sinopse = sinopse;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+}

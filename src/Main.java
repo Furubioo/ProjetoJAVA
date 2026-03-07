a46518fc699d492cd0c26fad69a7c3ba95215550
Flávio Daniel <epodomicehe@gmail.com>
2026-03-07 15:48:03 -0300

    Create Main.java
    Commit para início da classe Main

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Filme f = new Filme("Matrix", 136, "Sci-fi", 30.0);
+        Sessao s = new Sessao(f, "20:00");
+        
+    }
+}

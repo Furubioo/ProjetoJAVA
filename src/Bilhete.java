47b245e3ce872f6e42efb90e3f16460842afa641
Flávio Daniel <epodomicehe@gmail.com>
2026-03-07 11:28:36 -0300

    Create Bilhete.java
    Commit com os atributos, construtor e métodos getter e setter dos atributos(classe bilhete)

---
+++ src/Bilhete.java
@@ -0,0 +1,76 @@
+public class Bilhete {
+    private String user;
+    private int cpf;
+    private Sala sala;
+    private Sessao sessao;
+    private Filme filme;
+    private double valor;
+    private int[][] cadeiras = new int[10][15];  
+
+    public Bilhete(String user, int cpf, Sala sala, Sessao sessao, Filme filme, double valor, int[][] cadeiras) {
+        this.user = user;
+        this.cpf = cpf;
+        this.sala = sala;
+        this.sessao = sessao;
+        this.filme = filme;
+        this.valor = valor;
+        this.cadeiras = cadeiras;
+
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public Sala getSala() {
+        return sala;
+    }
+
+    public Sessao getSessao() {
+        return sessao;
+    }
+
+    public Filme getFilme() {
+        return filme;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public int[][] getCadeiras() {
+        return cadeiras;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setSala(Sala sala) {
+        this.sala = sala;
+    }
+
+    public void setSessao(Sessao sessao) {
+        this.sessao = sessao;
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public void setCadeiras(int[][] cadeiras) {
+        this.cadeiras = cadeiras;
+    }
+}

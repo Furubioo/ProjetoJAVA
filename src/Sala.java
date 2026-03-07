853d449480a8c2ebe1899f8680a06d8e374b1052
Claraapolinario_ <apolinariomariaclara436@gmail.com>
2026-03-07 15:19:13 -0300

    Update Sala.java
    commit para criação da classe Sala, seu atributo, seu construtor,  getter e setter.

--- src/Sala.java
+++ src/Sala.java
@@ -1,4 +1,18 @@
 public class Sala {
-    private Sessao[] 
+    private Sessao[] listaSessoes;
+
+    public Sala(int totalSessoes){
+        this.listaSessoes = new Sessao[totalSessoes];
+    }
+
+    public Sessao[] getSessao(){
+        return listaSessoes;
+    }
+
+    public void setSessao(Sessao[] listaSessoes){
+        this.listaSessoes = listaSessoes;
+    }
+
+    
     
 }

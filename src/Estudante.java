0391e57bccdf536f95a8aa38eb32654c311ceb76
Claraapolinario_ <apolinariomariaclara436@gmail.com>
2026-03-28 11:26:40 -0300

    Update Estudante.java
    Commit iniciando a criação da classe Estudante.

---
+++ src/Estudante.java
@@ -0,0 +1,9 @@
+public class Estudante extends Usuario {
+
+    public Estudante(String user,String cpf, String senha, int idade,char sexo,String email,String nomeCartao,String numeroCartao,String codigoCartao){
+        super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao);
+    }
+    
+
+    
+}

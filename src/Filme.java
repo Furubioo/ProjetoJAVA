12a4db9439451bf157aa5cb5302aaca8e531e394
Flávio Daniel <epodomicehe@gmail.com>
2026-03-28 12:03:33 -0300

    Update Filme.java
    Adição dos dois atributos nota e quantidade_criticos à classe Filme, além de reorganização dos gets e sets e construtor

--- src/Filme.java
+++ src/Filme.java
@@ -3,44 +3,80 @@ public class Filme {
     private int duracao;
     private String sinopse;
     private double valor;
-
-    public Filme(String nome, int duracao, String sinopse, double valor) {
+    private double nota;
+    private int quantidade_criticos;
+    
+    
+    public Filme(String nome, int duracao, String sinopse, double valor, double nota, int quantidade_criticos) {
         this.nome = nome;
         this.duracao = duracao;
         this.sinopse = sinopse;
         this.valor = valor;
+        this.nota = nota;
+        this.quantidade_criticos = quantidade_criticos;
     }
+
 
     public String getNome() {
         return nome;
     }
-    
-    public int getDuracao() {
-        return duracao;
-    }
 
-    public String getSinopse() {
-        return sinopse;
-    }
-
-    public double getValor() {
-        return valor;
-    }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+
     public void setDuracao(int duracao) {
         this.duracao = duracao;
     }
+
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
 
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
     }
 
+
+    public double getValor() {
+        return valor;
+    }
+
+
     public void setValor(double valor) {
         this.valor = valor;
     }
+
+
+    public double getNota() {
+        return nota;
+    }
+
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+
+
+    public int getQuantidade_criticos() {
+        return quantidade_criticos;
+    }
+
+
+    public void setQuantidade_criticos(int quantidade_criticos) {
+        this.quantidade_criticos = quantidade_criticos;
+    }
+
+
+
 
 } 

0c554e39eca81508040acca352d076513c1e074f
Flávio Daniel <epodomicehe@gmail.com>
2026-03-28 12:01:32 -0300

    Update Estudante.java
    Adição do Override também na classe Estudante

--- src/Estudante.java
+++ src/Estudante.java
@@ -4,6 +4,7 @@ public class Estudante extends Usuario {
         super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao);
     }
 
+    @Override // Para evitar a criação de um método novo em vez de sobrescrever o antigo
     public double calcularPrecoFinal(double precoBase){
         return precoBase / 2.0;
 

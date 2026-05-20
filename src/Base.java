7c5ed4cd8595e551e400745630db83471d5d3ac6
Flávio Daniel <epodomicehe@gmail.com>
2026-05-20 18:40:41 -0300

    Update Base.java
    

--- src/Base.java
+++ src/Base.java
@@ -11,14 +11,26 @@ public abstract class Base {
     }
 
     // Getters e Setters
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
+    public String getNome() {
+        return nome; 
+    }
+    public void setNome(String nome) { 
+        this.nome = nome; 
+    }
 
-    public int getIdade() { return idade; }
-    public void setIdade(int idade) { this.idade = idade; }
+    public int getIdade() { 
+        return idade; 
+    }
+    public void setIdade(int idade) { 
+        this.idade = idade; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+        return email; 
+    }
+    public void setEmail(String email) { 
+        this.email = email; 
+    }
 
     // Métodos abstratos
     public abstract void adicionarUsuario();

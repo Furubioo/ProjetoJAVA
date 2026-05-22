760e1f6de27feb73092f1a691e33d9af87cf2df5
Claraapolinario_ <apolinariomariaclara436@gmail.com>
2026-05-22 19:34:46 -0300

    Update Critico.java

--- src/Critico.java
+++ src/Critico.java
@@ -1,7 +1,8 @@
-public class Critico extends Usuario{
+public class Critico extends Usuario {
     private String origem;
 
-    public Critico(String user, String cpf, String senha, int idade, char sexo, String email, String nomeCartao, String numeroCartao, String codigoCartao, String origem) {
+    public Critico(String user, String cpf, String senha, int idade, char sexo, String email,
+                   String nomeCartao, String numeroCartao, String codigoCartao, String origem) {
         super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao);
         this.origem = origem;
     }
@@ -14,29 +15,24 @@ public class Critico extends Usuario{
         this.origem = origem;
     }
 
-    @Override 
-    public double calcularPrecoFinal(double precoBase){
-        precoBase = 0;
-        return precoBase;
+    @Override
+    public double calcularPrecoFinal(double precoBase) {
+        return 0;
     }
 
-    
     public void atribuirNota(Filme filme, double nota) {
+        double somaAtual = filme.getNota() * filme.getQuantidade_criticos();
         filme.setQuantidade_criticos(filme.getQuantidade_criticos() + 1);
-        filme.setNota((filme.getNota() + nota) / filme.getQuantidade_criticos());
-
+        filme.setNota((somaAtual + nota) / filme.getQuantidade_criticos());
     }
 
-    public void atribuirCritica(Filme filme, String critica) {
-        String[] vet = filme.getVetorcriticas();
-        
-        for (int i = vet.length-1; i >= 0; i--) {
+    public void atribuirCritica(Filme filme, String mensagem) {
+        Critica[] vet = filme.getVetorcriticas();
+        for (int i = 0; i < vet.length; i++) {
             if (vet[i] == null) {
-                vet[i] = critica;
+                vet[i] = new Critica(getUser(), this.origem, mensagem);
                 break;
             }
         }
     }
-
-
 }

14a240e5493ae1f7d37da14ca5c27b12d7089562
Claraapolinario_ <apolinariomariaclara436@gmail.com>
2026-05-22 19:34:59 -0300

    Update PersistenciaArquivo.java

--- src/PersistenciaArquivo.java
+++ src/PersistenciaArquivo.java
@@ -7,18 +7,19 @@ public class PersistenciaArquivo {
 
     public static void salvarFilmes(Filme[] filmes, int qtd) {
         try {
-            printWriter escritor = new PrintWriter(new FileWriter(ARQUIVO_FILMES));
+            PrintWriter escritor = new PrintWriter(new FileWriter(ARQUIVO_FILMES));
 
             for (int i = 0; i < qtd; i++) {
                 Filme f = filmes[i];
-                escritor.println(f.getNome() + "|" + f.getDuracao() + "|" + f.getSinopse().replace("|", " ") + f.getValor());
+                escritor.println(f.getNome() + "|" + f.getDuracao() + "|" +
+                        f.getSinopse().replace("|", " ") + "|" + f.getValor());
             }
 
             escritor.close();
-            System.out.println("Filmes salvos: " + qtd + "registro(s).");
-        
+            System.out.println("Filmes salvos: " + qtd + " registro(s).");
+
         } catch (IOException e) {
-            System.out.println("Erro ao salvar filmes: " +e.getMessage());
+            System.out.println("Erro ao salvar filmes: " + e.getMessage());
         }
     }
 
@@ -39,7 +40,7 @@ public class PersistenciaArquivo {
                 }
             }
             leitor.close();
-            System.out.println("Filmes carregados: "+qtd+ "registro(s).");
+            System.out.println("Filmes carregados: " + qtd + " registro(s).");
 
         } catch (IOException e) {
             System.out.println("Arquivo de filmes não encontrado. Iniciando vazio.");
@@ -51,20 +52,20 @@ public class PersistenciaArquivo {
 
     public static void salvarUsuarios(Usuario[] usuarios, int qtd) {
         try {
-            printWriter escritor = new PrintWriter(new FileWriter(ARQUIVO_USUARIOS));
+            PrintWriter escritor = new PrintWriter(new FileWriter(ARQUIVO_USUARIOS));
 
             for (int i = 0; i < qtd; i++) {
                 Usuario u = usuarios[i];
-
-                escritor.println(u.getUser() + "|" + u.getCpf() + "|" + u.getSenha() + "|" + u.getIdade() + "|"
-                + u.getSexo() + "|" + u.getEmail() + "|" + u.getNomecartao() + "|" + u.getNumerocartao() + "|" + u.getCodigocartao());
+                escritor.println(u.getUser() + "|" + u.getCpf() + "|" + u.getSenha() + "|" +
+                        u.getIdade() + "|" + u.getSexo() + "|" + u.getEmail() + "|" +
+                        u.getNomecartao() + "|" + u.getNumerocartao() + "|" + u.getCodigocartao());
             }
 
             escritor.close();
-            System.out.println("Usuarios salvos: " + qtd + " registro(s).");
+            System.out.println("Usuários salvos: " + qtd + " registro(s).");
 
         } catch (IOException e) {
-            System.out.println("Erro ao salvar usuários: "+e.getMessage());
+            System.out.println("Erro ao salvar usuários: " + e.getMessage());
         }
     }
 
@@ -80,22 +81,19 @@ public class PersistenciaArquivo {
                 String[] c = linha.split("\\|");
 
                 if (c.length == 9) {
-                    usuarios[qtd] = new Usuario(C[0], c[1], c[2], Integer.parseInt(c[3]), c[4].charAt(0),
-                    c[5], c[6], c[7], c[8]);
+                    usuarios[qtd] = new Usuario(c[0], c[1], c[2], Integer.parseInt(c[3]),
+                            c[4].charAt(0), c[5], c[6], c[7], c[8]);
                     qtd++;
                 }
             }
             leitor.close();
-            System.out.println("Usuários carregados: "+qtd+" registro(s).");
+            System.out.println("Usuários carregados: " + qtd + " registro(s).");
 
-        } catch(IOException e) {
+        } catch (IOException e) {
             System.out.println("Arquivo de usuários não encontrado. Iniciando vazio.");
         }
 
         qtdLida[0] = qtd;
         return usuarios;
     }
-
-
 }
-

ec547e1d7278527d78a7ed51adbece311ba6e1d3
Claraapolinario_ <apolinariomariaclara436@gmail.com>
2026-05-22 19:35:04 -0300

    Update Produto.java

--- src/Produto.java
+++ src/Produto.java
@@ -1,16 +1,24 @@
 public enum Produto {
-    PIPOCA_PEQUENA(10.0),
-    PIPOCA_GRANDE(15.0),
-    REFRIGERANTE_300ML(8.0),
-    REFRIGERANTE_500ML(10.0);
+    PIPOCA_PEQUENA("Pipoca Pequena", 10.0),
+    PIPOCA_GRANDE("Pipoca Grande", 15.0),
+    REFRIGERANTE_300ML("Refrigerante 300ml", 8.0),
+    REFRIGERANTE_500ML("Refrigerante 500ml", 10.0),
+    CHOCOLATE("Chocolate", 7.0),
+    AGUA_MINERAL("Água Mineral", 5.0);
 
-    private double preco;
+    private final String nome;
+    private final double preco;
 
-    Produto(double preco){
+    Produto(String nome, double preco) {
+        this.nome = nome;
         this.preco = preco;
     }
 
-    public double getPreco(){
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
         return preco;
     }
 }

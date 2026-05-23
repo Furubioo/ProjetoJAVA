b6394fcdc13dcb871436e07ffbd222f6d5e31d4f
Claraapolinario_ <apolinariomariaclara436@gmail.com>
2026-05-23 11:05:09 -0300

    Update Critico.java

--- src/model/Critico.java
+++ src/model/Critico.java
@@ -42,9 +42,15 @@ public class Critico extends Usuario {
 
         System.out.println("Limite de criticas atingido para este filme.");
     }
+    @Override
+    public Bilhete comprarBilhete(Sala sala, Sessao sessao, int linha, int coluna) {
+        return criarBilhete(sala, sessao, linha, coluna, 0.0);
+    }
 
     @Override
     public String toString() {
         return super.toString() + " [Critico | Origem: " + origem + "]";
     }
+
+
 }

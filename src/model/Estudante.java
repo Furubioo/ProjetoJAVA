d21c9f2b1aa4636375f8d76225be561929c95e95
Flávio Daniel <epodomicehe@gmail.com>
2026-05-22 21:35:02 -0300

    Create Estudante.java

---
+++ src/model/Estudante.java
@@ -0,0 +1,18 @@
+package model;
+
+public class Estudante extends Usuario {
+
+    public Estudante(String user, String cpf, String senha, int idade, char sexo, String email, String nomeCartao, String numeroCartao, String codigoCartao) {
+        super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao);
+    }
+
+    @Override
+    public double calcularPrecoFinal(double precoBase) {
+        return precoBase / 2.0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " [Estudante]";
+    }
+}

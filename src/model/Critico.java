d2d76ffdef1aab5aa297d9a16fa007ec20054a44
Flávio Daniel <epodomicehe@gmail.com>
2026-05-23 10:53:31 -0300

    Update Critico.java

--- src/model/Critico.java
+++ src/model/Critico.java
@@ -1,43 +1,50 @@
 package model;
 
+import model.strategy.PrecoCritico;
+
 public class Critico extends Usuario {
     private String origem;
 
-    public Critico(String user, String cpf, String senha, int idade, char sexo, String email, String nomeCartao, String numeroCartao, String codigoCartao, String origem) {
-        super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao);
+    public Critico(String user, String cpf, String senha, int idade, char sexo, String email,
+        String nomeCartao, String numeroCartao, String codigoCartao, String origem) {
+        super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao, new PrecoCritico());
         this.origem = origem;
     }
 
     public String getOrigem() {
-        return origem; 
-    }
-    public void setOrigem(String origem) { 
-        this.origem = origem; 
+        return origem;
     }
 
-    @Override
-    public double calcularPrecoFinal(double precoBase) {
-        return 0; 
+    public void setOrigem(String origem) {
+        this.origem = origem;
     }
 
     public void atribuirNota(Filme filme, double nota) {
+        if (nota < 0 || nota > 10) {
+            System.out.println("Nota invalida. Informe uma nota entre 0 e 10.");
+            return;
+        }
+
         double somaAtual = filme.getNota() * filme.getQuantidade_criticos();
         filme.setQuantidade_criticos(filme.getQuantidade_criticos() + 1);
         filme.setNota((somaAtual + nota) / filme.getQuantidade_criticos());
     }
 
     public void atribuirCritica(Filme filme, String mensagem) {
-        Critica[] vet = filme.getVetorcriticas();
-        for (int i = 0; i < vet.length; i++) {
-            if (vet[i] == null) {
-                vet[i] = new Critica(getUser(), this.origem, mensagem);
-                break;
+        Critica[] criticas = filme.getVetorcriticas();
+
+        for (int i = 0; i < criticas.length; i++) {
+            if (criticas[i] == null) {
+                criticas[i] = new Critica(getUser(), origem, mensagem);
+                return;
             }
         }
+
+        System.out.println("Limite de criticas atingido para este filme.");
     }
 
     @Override
     public String toString() {
-        return super.toString() + " [Crítico | Origem: " + origem + "]";
+        return super.toString() + " [Critico | Origem: " + origem + "]";
     }
 }

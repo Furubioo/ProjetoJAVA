68c0385bc6055be12c7ffbbcc4a034fd0f12f396
Flávio Daniel <epodomicehe@gmail.com>
2026-05-23 10:53:29 -0300

    Update Estudante.java

--- src/model/Estudante.java
+++ src/model/Estudante.java
@@ -1,14 +1,12 @@
 package model;
 
+import model.strategy.PrecoEstudante;
+
 public class Estudante extends Usuario {
 
-    public Estudante(String user, String cpf, String senha, int idade, char sexo, String email, String nomeCartao, String numeroCartao, String codigoCartao) {
-        super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao);
-    }
-
-    @Override
-    public double calcularPrecoFinal(double precoBase) {
-        return precoBase / 2.0;
+    public Estudante(String user, String cpf, String senha, int idade, char sexo, String email,
+        String nomeCartao, String numeroCartao, String codigoCartao) {
+        super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao, new PrecoEstudante());
     }
 
     @Override

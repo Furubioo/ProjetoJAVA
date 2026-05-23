4588aa07e6a94d7bc5e9a168d7006b9ae9ffcb22
Flávio Daniel <epodomicehe@gmail.com>
2026-05-22 21:35:21 -0300

    Create Sessao.java

---
+++ src/model/Sessao.java
@@ -0,0 +1,66 @@
+package model;
+
+public class Sessao {
+    private Filme filme;
+    private String horario;
+    private boolean[][] cadeiras;
+    private boolean emCartaz;
+
+    public Sessao(Filme filme, String horario) {
+        this.filme    = filme;
+        this.horario  = horario;
+        this.cadeiras = new boolean[10][15];
+        this.emCartaz = true;
+    }
+
+    public Filme getFilme() {
+        return filme; 
+    }
+
+    public void setFilme(Filme filme) {
+        this.filme = filme; 
+    }
+
+    public String getHorario() {
+        return horario; 
+    }
+
+    public void setHorario(String horario) { 
+        this.horario = horario;
+    }
+
+    public boolean[][] getCadeiras() {
+        return cadeiras; 
+    }
+
+    public void setCadeiras(boolean[][] cadeiras) { 
+        this.cadeiras = cadeiras;
+    }
+
+    public boolean isEmCartaz() {
+        return emCartaz; 
+    }
+
+    public void setEmCartaz(boolean emCartaz) {
+        this.emCartaz = emCartaz; 
+    }
+
+    public boolean cadeiraDisponivel(int linha, int coluna) {
+        return !cadeiras[linha][coluna];
+    }
+
+    public void ocuparCadeira(int linha, int coluna) {
+        cadeiras[linha][coluna] = true;
+    }
+
+    public String formatarCadeira(int linha, int coluna) {
+        return String.valueOf((char) ('A' + linha)) + (coluna + 1);
+    }
+
+    @Override
+    public String toString() {
+        return "Sessão[" + (filme != null ? filme.getNome() : "sem filme") +
+               " | " + horario +
+               " | " + (emCartaz ? "em cartaz" : "encerrada") + "]";
+    }
+}

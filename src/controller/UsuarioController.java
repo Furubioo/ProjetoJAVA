601983e4e2770cd09aaa5a66fc7c0861a321bd3f
Flávio Daniel <epodomicehe@gmail.com>
2026-05-22 21:33:57 -0300

    Update UsuarioController.java

--- src/controller/UsuarioController.java
+++ src/controller/UsuarioController.java
@@ -10,13 +10,13 @@ public class UsuarioController {
     private static final int MAX = 100;
 
     public UsuarioController() {
-        this.usuarios = new Usuario[MAX];
+        this.usuarios    = new Usuario[MAX];
         this.qtdUsuarios = 0;
     }
 
     public void carregarUsuarios() {
-        int[] qtdLida = new int[1];
-        this.usuarios = PersistenciaArquivo.carregarUsuarios(qtdLida);
+        int[] qtdLida    = new int[1];
+        this.usuarios    = PersistenciaArquivo.carregarUsuarios(qtdLida);
         this.qtdUsuarios = qtdLida[0];
     }
 
@@ -36,32 +36,29 @@ public class UsuarioController {
     public void excluirUsuario(String email) {
         for (int i = 0; i < qtdUsuarios; i++) {
             if (usuarios[i].getEmail().equalsIgnoreCase(email)) {
-                for (int j = i; j < qtdUsuarios - 1; j++) {
-                    usuarios[j] = usuarios[j + 1];
-                }
-                usuarios[--qtdUsuarios] = null;
-                salvarUsuarios();
-                System.out.println("Usuário removido: " + email);
-                return;
+                for (int j = i; j < qtdUsuarios - 1; j++) usuarios[j] = usuarios[j + 1];
+                    usuarios[--qtdUsuarios] = null;
+                    salvarUsuarios();
+                    System.out.println("Usuário removido: " + email);
+                    return;
             }
         }
         System.out.println("Usuário não encontrado: " + email);
     }
 
     public Usuario buscarPorLogin(String user, String senha) {
-        for (int i = 0; i < qtdUsuarios; i++) {
-            if (usuarios[i].getUser().equals(user) && usuarios[i].getSenha().equals(senha)) {
+        for (int i = 0; i < qtdUsuarios; i++)
+            if (usuarios[i].getUser().equals(user) && usuarios[i].getSenha().equals(senha))
                 return usuarios[i];
-            }
-        }
         return null;
     }
 
-    public Usuario[] getUsuarios() {
-        return usuarios;
+    public Usuario[] getUsuarios() { 
+        return usuarios; 
+    }
+    
+    public int getQtdUsuarios() {
+        return qtdUsuarios; 
     }
 
-    public int getQtdUsuarios() {
-        return qtdUsuarios;
-    }
 }

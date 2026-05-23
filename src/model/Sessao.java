7bd3d66e010b98af6cead848fcb0375aa103705d
Flávio Daniel <epodomicehe@gmail.com>
2026-05-23 10:53:21 -0300

    Update Sessao.java

--- src/model/Sessao.java
+++ src/model/Sessao.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.time.LocalTime;
+import java.time.format.DateTimeParseException;
+
 public class Sessao {
     private Filme filme;
     private String horario;
@@ -7,60 +10,79 @@ public class Sessao {
     private boolean emCartaz;
 
     public Sessao(Filme filme, String horario) {
-        this.filme    = filme;
-        this.horario  = horario;
+        this.filme = filme;
+        this.horario = horario;
         this.cadeiras = new boolean[10][15];
         this.emCartaz = true;
     }
 
     public Filme getFilme() {
-        return filme; 
+        return filme;
     }
 
     public void setFilme(Filme filme) {
-        this.filme = filme; 
+        this.filme = filme;
     }
 
     public String getHorario() {
-        return horario; 
+        return horario;
     }
 
-    public void setHorario(String horario) { 
+    public void setHorario(String horario) {
         this.horario = horario;
     }
 
     public boolean[][] getCadeiras() {
-        return cadeiras; 
+        return cadeiras;
     }
 
-    public void setCadeiras(boolean[][] cadeiras) { 
+    public void setCadeiras(boolean[][] cadeiras) {
         this.cadeiras = cadeiras;
     }
 
     public boolean isEmCartaz() {
-        return emCartaz; 
+        return emCartaz;
     }
 
     public void setEmCartaz(boolean emCartaz) {
-        this.emCartaz = emCartaz; 
+        this.emCartaz = emCartaz;
     }
 
     public boolean cadeiraDisponivel(int linha, int coluna) {
+        if (!posicaoValida(linha, coluna)) {
+            return false;
+        }
+
         return !cadeiras[linha][coluna];
     }
 
     public void ocuparCadeira(int linha, int coluna) {
-        cadeiras[linha][coluna] = true;
+        if (posicaoValida(linha, coluna)) {
+            cadeiras[linha][coluna] = true;
+        }
+    }
+
+    public boolean horarioJaPassou() {
+        try {
+            LocalTime inicio = LocalTime.parse(horario.trim());
+            return LocalTime.now().isAfter(inicio);
+        } catch (DateTimeParseException e) {
+            return false;
+        }
     }
 
     public String formatarCadeira(int linha, int coluna) {
         return String.valueOf((char) ('A' + linha)) + (coluna + 1);
     }
 
+    private boolean posicaoValida(int linha, int coluna) {
+        return linha >= 0 && linha < cadeiras.length && coluna >= 0 && coluna < cadeiras[linha].length;
+    }
+
     @Override
     public String toString() {
-        return "Sessão[" + (filme != null ? filme.getNome() : "sem filme") +
-               " | " + horario +
-               " | " + (emCartaz ? "em cartaz" : "encerrada") + "]";
+        return "Sessao[" + (filme != null ? filme.getNome() : "sem filme") +
+        " | " + horario +
+        " | " + (emCartaz ? "em cartaz" : "encerrada") + "]";
     }
 }

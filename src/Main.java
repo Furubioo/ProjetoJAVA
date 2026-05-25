2ed1f983c4253be3bbd8392210e741ee25571724
Claraapolinario_ <apolinariomariaclara436@gmail.com>
2026-05-25 14:06:59 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ import java.io.PrintStream;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Garante saída UTF-8 no terminal (resolve o "?" em Windows)
         System.setOut(new PrintStream(System.out, true, "UTF-8"));
         System.setErr(new PrintStream(System.err, true, "UTF-8"));
 

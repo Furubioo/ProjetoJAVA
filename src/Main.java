43dc90392d83ee50c201f5149ef518f25fea2dbf
Flávio Daniel <epodomicehe@gmail.com>
2026-05-24 23:28:23 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import controller.CinemaController;
 import controller.FilmeController;
 import controller.UsuarioController;
-import model.Filme;
 import view.CinemaView;
 
+import java.io.PrintStream;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // Garante saída UTF-8 no terminal (resolve o "?" em Windows)
+        System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        System.setErr(new PrintStream(System.err, true, "UTF-8"));
+
         FilmeController filmeController = new FilmeController();
         UsuarioController usuarioController = new UsuarioController();
-        CinemaController cinemaController = new CinemaController();
+        CinemaController cinemaController = new CinemaController(filmeController);
 
         filmeController.carregarFilmes();
         usuarioController.carregarUsuarios();

d535bf85998368e3a3b3b6ad9a314484e752b5ba
Flávio Daniel <epodomicehe@gmail.com>
2026-05-26 23:18:29 -0300

    Update Critico.java

--- src/model/Critico.java
+++ src/model/Critico.java
@@ -11,13 +11,8 @@ public class Critico extends Usuario {
         this.origem = origem;
     }
 
-    public String getOrigem() {
-        return origem;
-    }
-
-    public void setOrigem(String origem) {
-        this.origem = origem;
-    }
+    public String getOrigem() { return origem; }
+    public void setOrigem(String origem) { this.origem = origem; }
 
     public void atribuirNota(Filme filme, double nota) {
         if (nota < 0 || nota > 10) {
@@ -30,12 +25,12 @@ public class Critico extends Usuario {
         filme.setNota((somaAtual + nota) / filme.getQuantidade_criticos());
     }
 
-    public void atribuirCritica(Filme filme, String mensagem) {
+    public void atribuirCritica(String critica, Filme filme) {
         Critica[] criticas = filme.getVetorcriticas();
 
         for (int i = 0; i < criticas.length; i++) {
             if (criticas[i] == null) {
-                criticas[i] = new Critica(getUser(), origem, mensagem);
+                criticas[i] = new Critica(getUser(), origem, critica);
                 return;
             }
         }
@@ -43,10 +38,18 @@ public class Critico extends Usuario {
         System.out.println("Limite de criticas atingido para este filme.");
     }
 
+    public void atribuirCritica(Filme filme, String critica) {
+        atribuirCritica(critica, filme);
+    }
+    
+    @Override
+    public Bilhete comprarBilhete(Sala sala, Sessao sessao, int linha, int coluna) {
+        double valorFinal = 0.0; // entrada gratuita garantida
+        return criarBilhete(sala, sessao, linha, coluna, valorFinal);
+    }
+
     @Override
     public String toString() {
         return super.toString() + " [Critico | Origem: " + origem + "]";
     }
-
-
 }

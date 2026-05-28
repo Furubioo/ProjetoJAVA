c4ac852d63b83d73331cdb3b31eb410e558cfee0
Flávio Daniel <epodomicehe@gmail.com>
2026-05-27 22:11:35 -0300

    Update GerenciaDeFilmes.java

--- src/model/GerenciaDeFilmes.java
+++ src/model/GerenciaDeFilmes.java
@@ -2,6 +2,8 @@ package model;
 
 public interface GerenciaDeFilmes {
     void incluirFilme(Filme filme);
+
     void excluirFilme(String tituloFilme);
+
     void alterarFilme(String tituloFilme, Filme novosDados);
 }

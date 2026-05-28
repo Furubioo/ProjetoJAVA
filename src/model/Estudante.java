8fea7dff79a0d1abdc15ae59fd039971105e6f76
Flávio Daniel <epodomicehe@gmail.com>
2026-05-27 22:11:41 -0300

    Update Estudante.java

--- src/model/Estudante.java
+++ src/model/Estudante.java
@@ -5,7 +5,7 @@ import model.strategy.PrecoEstudante;
 public class Estudante extends Usuario {
 
     public Estudante(String user, String cpf, String senha, int idade, char sexo, String email,
-        String nomeCartao, String numeroCartao, String codigoCartao) {
+            String nomeCartao, String numeroCartao, String codigoCartao) {
         super(user, cpf, senha, idade, sexo, email, nomeCartao, numeroCartao, codigoCartao, new PrecoEstudante());
     }
 

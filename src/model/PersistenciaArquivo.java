308dbce2a963e94af980bc273dc2fedb75273ff6
Flávio Daniel <epodomicehe@gmail.com>
2026-05-27 22:11:33 -0300

    Update PersistenciaArquivo.java

--- src/model/PersistenciaArquivo.java
+++ src/model/PersistenciaArquivo.java
@@ -6,21 +6,20 @@ public class PersistenciaArquivo {
 
     private static final String BASE_DIR = System.getProperty("user.dir");
     private static final String ARQUIVO_USUARIOS = BASE_DIR + java.io.File.separator + "usuarios.txt";
-    private static final String ARQUIVO_FILMES   = BASE_DIR + java.io.File.separator + "filmes.txt";
-    private static final int    MAX              = 100;
+    private static final String ARQUIVO_FILMES = BASE_DIR + java.io.File.separator + "filmes.txt";
+    private static final int MAX = 100;
 
     public static void salvarFilmes(Filme[] filmes, int qtd) {
         try (PrintWriter escritor = new PrintWriter(new FileWriter(ARQUIVO_FILMES))) {
             for (int i = 0; i < qtd; i++) {
                 Filme f = filmes[i];
                 escritor.println(
-                    f.getNome()                           + "|" +
-                    f.getDuracao()                        + "|" +
-                    f.getSinopse().replace("|", " ")      + "|" +
-                    f.getValor()                          + "|" +
-                    f.getNota()                           + "|" +
-                    f.getQuantidade_criticos()
-                );
+                        f.getNome() + "|" +
+                                f.getDuracao() + "|" +
+                                f.getSinopse().replace("|", " ") + "|" +
+                                f.getValor() + "|" +
+                                f.getNota() + "|" +
+                                f.getQuantidade_criticos());
             }
         } catch (IOException e) {
             System.out.println("Erro ao salvar filmes: " + e.getMessage());
@@ -37,7 +36,7 @@ public class PersistenciaArquivo {
                 String[] c = linha.split("\\|");
                 if (c.length >= 4) {
                     Filme f = new Filme(c[0], Integer.parseInt(c[1]),
-                                        c[2], Double.parseDouble(c[3]));
+                            c[2], Double.parseDouble(c[3]));
                     if (c.length >= 6) {
                         f.setNota(Double.parseDouble(c[4]));
                         f.setQuantidade_criticos(Integer.parseInt(c[5]));
@@ -66,17 +65,16 @@ public class PersistenciaArquivo {
                 }
 
                 escritor.println(
-                    u.getUser()         + "|" +
-                    u.getCpf()          + "|" +
-                    u.getSenha()        + "|" +
-                    u.getIdade()        + "|" +
-                    u.getSexo()         + "|" +
-                    u.getEmail()        + "|" +
-                    u.getNomecartao()   + "|" +
-                    u.getNumerocartao() + "|" +
-                    u.getCodigocartao() + "|" +
-                    tipo
-                );
+                        u.getUser() + "|" +
+                                u.getCpf() + "|" +
+                                u.getSenha() + "|" +
+                                u.getIdade() + "|" +
+                                u.getSexo() + "|" +
+                                u.getEmail() + "|" +
+                                u.getNomecartao() + "|" +
+                                u.getNumerocartao() + "|" +
+                                u.getCodigocartao() + "|" +
+                                tipo);
             }
         } catch (IOException e) {
             System.out.println("Erro ao salvar usuários: " + e.getMessage());
@@ -91,13 +89,14 @@ public class PersistenciaArquivo {
             String linha;
             while ((linha = leitor.readLine()) != null && qtd < MAX) {
                 String[] c = linha.split("\\|");
-                if (c.length < 9) continue;
+                if (c.length < 9)
+                    continue;
 
                 String user = c[0];
                 String cpf = c[1];
                 String senha = c[2];
-                int    idade = Integer.parseInt(c[3]);
-                char   sexo  = c[4].charAt(0);
+                int idade = Integer.parseInt(c[3]);
+                char sexo = c[4].charAt(0);
                 String email = c[5];
                 String nomeCartao = c[6];
                 String numCartao = c[7];
@@ -108,13 +107,13 @@ public class PersistenciaArquivo {
                 if (tipoStr.startsWith("CRITICO:")) {
                     String origem = tipoStr.substring(8);
                     u = new Critico(user, cpf, senha, idade, sexo, email,
-                                    nomeCartao, numCartao, codCartao, origem);
+                            nomeCartao, numCartao, codCartao, origem);
                 } else if (tipoStr.equalsIgnoreCase("ESTUDANTE")) {
                     u = new Estudante(user, cpf, senha, idade, sexo, email,
-                                      nomeCartao, numCartao, codCartao);
+                            nomeCartao, numCartao, codCartao);
                 } else {
                     u = new Usuario(user, cpf, senha, idade, sexo, email,
-                                    nomeCartao, numCartao, codCartao);
+                            nomeCartao, numCartao, codCartao);
                 }
                 usuarios[qtd++] = u;
             }
